192b06aadfbe523d5cab2564b55857742d79b2ca
Administrator <1972689216@qq.com>
2020-07-27 17:48:07 +0800

    123

--- src/main/java/com/item/controller/UnityController.java
+++ src/main/java/com/item/controller/UnityController.java
@@ -1,20 +1,25 @@
 package com.item.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.item.entity.Page;
 import com.item.entity.PakInfo;
 import com.item.service.UnityService;
+import com.item.service.UserService;
 import com.item.tool.Result;
 
-@RestController
+@Controller
 @RequestMapping("unity")
 public class UnityController {
 
 	@Autowired
 	private UnityService unityService;
+
+	@Autowired
+	private UserService userService;
 
 	/**
 	 * 通过名称模糊查询
@@ -24,6 +29,7 @@ public class UnityController {
 	 * @return
 	 */
 	@RequestMapping(value = "/queryPakInfoByDisplayname")
+	@ResponseBody
 	public Result<?> queryPakInfoByDisplayname(PakInfo pakInfo, Page page) {
 		page = unityService.queryPakInfoByDisplayname(pakInfo, page);
 		return Result.success(page);
@@ -37,6 +43,7 @@ public class UnityController {
 	 * @return
 	 */
 	@RequestMapping(value = "/queryPakInfoByName")
+	@ResponseBody
 	public Result<?> queryPakInfoByName(String name, Page page) {
 		page = unityService.queryPakInfoByName(name, page);
 		return Result.success(page);
@@ -50,6 +57,7 @@ public class UnityController {
 	 * @return
 	 */
 	@RequestMapping(value = "/queryFlowModelByName")
+	@ResponseBody
 	public Result<?> queryFlowModelByName(String name, Page page, String uid) {
 		page = unityService.queryFlowModelByName(name, page, uid);
 		return Result.success(page);
@@ -61,7 +69,21 @@ public class UnityController {
 	 * @return
 	 */
 	@RequestMapping(value = "/queryZTree")
+	@ResponseBody
 	public Result<?> queryZTree() {
 		return unityService.queryZTree();
 	}
+
+	/**
+	 * ue页面跳转获取用户信息
+	 * 
+	 * @param username
+	 * @param sessionid
+	 * @return
+	 */
+	@RequestMapping(value = "/loginAdminByUserId")
+	public String loginAdminByUserId(String username, String password) {
+		return "uintylogin";
+	}
+
 }
